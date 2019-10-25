12caa4222254d6c10f788b06dec8ccc4160e6a94
Mike Shecket <mshecket@gmail.com>
2019-10-25 09:49:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ request.open('GET', 'https://helloworld.mikeshecket.com/helloworld.php', true)
 
 request.onload = function () {
     var data = JSON.parse(this.response)
+    document.getElementById("root").innerHTML = data.message
 }
 
 request.send()
-
-document.getElementById("root").innerHTML = data.message
