5c249294e4650dd5ae8c738be2dd91410cfd14d3
Mike Shecket <mshecket@gmail.com>
2019-10-25 10:45:03 -0400

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,24 @@
 <?php
-  $data = new stdClass();
-  $myJSON = new stdClass();
-  $data->message = "Hello world!";
+   $data = new stdClass();
+   $myJSON = new stdClass();
+
+   $dbhost = 'localhost';
+   $dbuser = 'hello';
+   $dbpass = 'phuubohngaipaip';
+   $dbname = 'mikeshecket_HELLOWORLD';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
+
+   $sql = "select * from messages";
+   $result = $conn->query($sql);
+   
+   mysqli_close($conn);
+
+
+  $data->message = mysqli_fetch_array($result)[0];
   $myJSON = json_encode($data);
   echo $myJSON;
 ?>
